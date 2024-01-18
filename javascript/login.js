d9cf7afd611a02f8b3429e2de6fa50913ee4b21d
TimWidl94 <TimNowak195@gmail.com>
2024-01-18 19:54:25 +0100

    JOIN-18 sign up html
    updatin js and css for sign up page

--- javascript/login.js
+++ javascript/login.js
@@ -1,16 +1,48 @@
-function signUp(){
-    let container = document.getElementById('loginScreenBody');
-    changeCssFromInput(container);
-    container.innerHTML = ``;
+function signUp() {
+  let container = document.getElementById("formBody");
+  changeCssFromInput(container);
+  document.getElementById("headerRightBox").classList.add("d-none");
 
-    container.innerHTML = `
+  container.innerHTML = `
+    <form action="" id="signUpBody" class="signUpBody">
+        <div class="h1Box dp-flex fd-colum">
+            <h1>Sign up</h1>
+            <img src="./assets/img/LogIn/Vector.png" alt="" />
+        </div>
+        <div class="dp-flex fd-colum">
+            <div class="inputFieldContainerSignUp">
+                <div class="inputFieldBox">
+                    <input type="text" class="inputField" placeholder="Name" />
+                    <img src="./assets/img/LogIn/person.svg" alt="" class="inputImgMail" />
+                </div>
+                <div class="inputFieldBox">
+                    <input type="text" class="inputField" placeholder="Email"/>
+                    <img src="./assets/img/LogIn/mail.svg" alt="" class="inputImgLock" />
+                </div>
+                <div class="inputFieldBox">
+                    <input type="text" class="inputField" placeholder="Password"/>
+                    <img src="./assets/img/LogIn/lock.svg" alt="" class="inputImgLock" />
+                </div>
+                <div class="inputFieldBox">
+                    <input type="text" class="inputField" placeholder="Confirm Password"/>
+                    <img src="./assets/img/LogIn/lock.svg" alt="" class="inputImgLock" />
+                </div>
+            </div>
+            <div class="checkboxPrivacyPolicy dp-flex">
+                <input type="checkbox" id="checkboxSavePassword" name="checkboxSavePassword" class="checkboxSavePassword"/>
+                <p>i accept the 
+                    <a href="./privacyPolice.html">Privacy policy</a>
+                </p>
+            </div>
+        </div>
+        <div class="ButtonBox dp-flex">
+            <button class="buttonGrey buttonLogin" onclick="logIn()">Sign up</button>
+        </div>
+    </form>
     
     
-    
-    `
+    `;
 }
 
-function changeCssFromInput(container){
-    container.style.width = "422px";
-    container.style.height = "534px";
+function changeCssFromInput(container) {
 }

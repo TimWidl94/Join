de73ce9fa2265219336ab0135f398c8e71f974e9
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-01-22 20:38:47 +0100

    Function openContactInfo() added

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -45,7 +45,7 @@ let contactColors = ['#FF7A00', '#9327FF', '#6E52FF', '#FC71FF', '#FFBB2B', '#1F
 
 function init() {
   loadContacts();
-  setBackgroundColor();
+  //   setBackgroundColor();
 }
 
 function loadContacts() {
@@ -53,29 +53,83 @@ function loadContacts() {
 
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-    let str = contact.name;
-    let acronym = str.split(/\s/).reduce((response, word) => (response += word.slice(0, 1)), '');
+    let acronym = getFirstLetters(contact.name);
 
     document.getElementById('basic-info-wrapper').innerHTML += /*html*/ `
-       <div id="contact-list-basic-info">
-      <div class="capital-letters-list" id="capital-letters-list${i}"> 
-            <p id="capital-letters-list">${acronym}</p>
-            </div>
-          <div id="name-and-mail"> 
-          <p id="name-list">${contact.name}</p>
+        <div id="contact-list-basic-info${i}" class="contact-list-basic-info" onclick="toggleBackground(${i}), openContactInfo(${i})">
+            <div class="capital-letters-list" id="capital-letters-list${i}"> 
+                <p id="capital-letters-list">${acronym}</p>
+                </div>
+            <div id="name-and-mail"> 
+            <p id="name-list${i}">${contact.name}</p>
               <p id="email-list">${contact.mail}</p>
           </div>
-          </div>
+        </div>
         `;
   }
 }
 
-function setBackgroundColor() {
-  let element = document.getElementById(`capital-letters-list${i}`);
-
-  for (let i = 0; i < contacts.length; i++) {
-    for (let j = 0; j < contactColors.length; j++) {
-      const element = array[j];
-    }
-  }
+function getFirstLetters(str) {
+  return str.split(/\s/).reduce((response, word) => (response += word.slice(0, 1)), '');
 }
+
+function openContactInfo(i) {
+  let contact = contacts[i];
+  let acronym = getFirstLetters(contact.name);
+  document.getElementById('contact-info').innerHTML = '';
+  document.getElementById('contact-info').innerHTML += /*html*/ `
+    <div id="basic-info">
+        <div class="capital-letters">
+            <h2 id="capital-letters">${acronym}</h2>
+        </div>
+        <div class="name-and-changes">
+            <h2 id="name">${contact.name}</h2>
+            <div class="changes">
+                <div class="edit" onclick="editContact(i)">
+                    <img src="assets/img/Contacts/edit.svg" alt="" />
+                    <p>Edit</p>
+                </div>
+                <div class="delete" onclick="deleteContact(i)">
+                    <img src="assets/img/Contacts/delete.svg" alt="" />
+                    <p>Delete</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contact-info-text">
+        <p>Contact Information</p>
+    </div>
+
+    <div class="mail-and-phone">
+        <div class="mail">
+            <p>Email</p>
+            <p id="contact-email">${contact.mail}</p>
+        </div>
+        <div class="phone">
+            <p>Phone</p>
+            <p id="contact-phone" type="phone">${contact.phone}</p>
+        </div>
+    </div>
+  `;
+}
+
+function toggleBackground(i) {
+  //   let removeBackground = document.getElementById('contact-list-basic-info');
+  //   removeBackground.forEach((element) => {
+  //     element.classList.remove('bg-primary');
+  //     console.log('removeBackground:', removeBackground);
+  //   });
+  document.getElementById(`contact-list-basic-info${i}`).classList.toggle('bg-primary');
+  document.getElementById(`name-list${i}`).classList.toggle('color-white');
+}
+
+// function setBackgroundColor() {
+//   let element = document.getElementById(`capital-letters-list${i}`);
+
+//   for (let i = 0; i < contacts.length; i++) {
+//     for (let j = 0; j < contactColors.length; j++) {
+//       const element = array[j];
+//     }
+//   }
+// }

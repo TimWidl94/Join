098cbe7792edb08387157c7bbd4201580508e6bc
TimWidl94 <TimNowak195@gmail.com>
2024-01-23 07:40:19 +0100

    JOIN-18 sign up html
    update remote log in function and delete localstorage function

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+const STORAGE_TOKEN = '1A3L76SPA4NC4PAYEHDAQA1YFXXKV47R442Z0HYA';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
 let users = [
-    {
-      username: "Guest",
-      email: "Guest@web.de",
-      password: "admin123",
-      checkPassword: "admin123",
-    },
+    // {
+      // username: "Guest",
+      // email: "Guest@web.de",
+      // password: "admin123",
+      // checkPassword: "admin123",
+    // },
   ];
 
 
 async function init(){
     await includeHTML();
+    await loadUsers();
     await loadLogIn();
-    loadLocalStorageData();
 }
 
 

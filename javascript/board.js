2594a8f846d27e16d73eeb13725fc985ff140f15
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-01-25 13:22:10 +0100

    JOIN-25 implement svg css hover
    -implement svg css hover animation, changing color
    - adding js for drag and drop and change markup for js
    -style improvements for desktop view

--- javascript/board.js
+++ javascript/board.js
@@ -1,6 +1,23 @@
+let todos = [{
+  'id': 0,
+  'title': 'Putzen',
+  'category': 'open'
+}, {
+  'id': 1,
+  'title': 'Kochen',
+  'category': 'open'
+}, {
+  'id': 2,
+  'title': 'Einkaufen',
+  'category': 'closed'
+}];
+let currentDraggedElement;
+
 function init(){
     loadData();
+    updateHTML();
 }
+
 function openAddTaskPopup() {
     document.getElementById('addTaskPopup').classList.remove('d-none');
     document.getElementById('addTaskPopup').classList.add('slide-in');
@@ -18,4 +35,45 @@ function openAddTaskPopup() {
         setTimeout(function () {
           addTaskPopup.classList.remove("slide-out");
         }, 900);
+}
+
+function startDragging(id) {
+  currentDraggedElement = id;
+}
+
+function updateHTML() {
+  let open = todos.filter(t => t['category'] == 'open');
+
+  document.getElementById('open').innerHTML = '';
+
+  for (let index = 0; index < open.length; index++) {
+      const element = open[index];
+      document.getElementById('open').innerHTML += generateTodoHTML(element);
   }
+
+  let closed = todos.filter(t => t['category'] == 'closed');
+
+  document.getElementById('closed').innerHTML = '';
+
+  for (let index = 0; index < closed.length; index++) {
+      const element = closed[index];
+      document.getElementById('closed').innerHTML += generateTodoHTML(element);
+  }
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function moveTo(category) {
+  todos[currentDraggedElement]['category'] = category;
+  updateHTML();
+}
+
+function highlight(id) {
+  document.getElementById(id).classList.add('drag-area-highlight');
+}
+
+function removeHighlight(id) {
+  document.getElementById(id).classList.remove('drag-area-highlight');
+}

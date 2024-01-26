1387d9d8c60b5200ae5d9f43a8b1f701f7fbcfec
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-01-26 11:05:34 +0100

    JOIN-19 Sorted data and pathes

--- templates/contactTemplate.js
+++ templates/contactTemplate.js
@@ -66,7 +66,7 @@ function editContactHTML(i) {
         <div class="edit-contact-container">
             <div id="edit-contact" onclick="doNotClose(event)">
             <section class="left-container">
-                <img src="assets/img/Contacts/logo-white.svg" alt="Logo" />
+                <img src="assets/img/icons/logo-white.svg" alt="Logo" />
                 <div class="left-card-text">
                 <h2>Edit Contact</h2>
                 </div>
@@ -75,22 +75,22 @@ function editContactHTML(i) {
 
             <section class="right-container">
                 <div class="profile">
-                <img id="profile-img" src="assets/img/Contacts/person.svg" alt="Profile Image" />
+                <img id="profile-img" src="assets/img/icons/person.svg" alt="Profile Image" />
                 </div>
 
-                <form class="inputButtonsWrapper" onsubmit="return saveEditedContact(${i});">
+                <form class="inputButtonsWrapper" onsubmit="saveEditedContact(${i}); return false">
                 <div class="inputFieldContainer height-unset">
                     <div class="inputFieldBox">
                     <input type="text" class="inputField" placeholder="Name" id="edit-name" required autocomplete="none" />
-                    <img src="assets/img/Contacts/person_grey.svg" alt="Person" class="inputImgPerson" />
+                    <img src="assets/img/icons/person.svg" alt="Person" class="inputImgPerson" />
                     </div>
                     <div class="inputFieldBox">
                     <input type="email" class="inputField" placeholder="Email" id="edit-mail" required autocomplete="none"/>
-                    <img src="assets/img/Contacts/mail.svg" alt="Mail" class="inputImgMail" />
+                    <img src="assets/img/icons/mail.svg" alt="Mail" class="inputImgMail" />
                     </div>
                     <div class="inputFieldBox">
                     <input type="tel" oninput="validatePhoneNumber(this)" class="inputField" placeholder="Phone" id="edit-tel" required autocomplete="none"/>
-                    <img src="assets/img/Contacts/call.svg" alt="Phone" class="inputImgPhone2" />
+                    <img src="assets/img/icons/call.svg" alt="Phone" class="inputImgPhone2" />
                     </div>
                 </div>
 
@@ -99,7 +99,7 @@ function editContactHTML(i) {
                     Delete <img src="assets/img/AddTask/cancel.svg" alt="Clear Icon"
                     /></button>
                     <button id="save" class="buttonGrey">
-                    Save <img src="assets/img/AddTask/check_white.svg" alt="Check Icon"
+                    Save <img src="assets/img/icons/close.svg" alt="Check Icon"
                     /></button>
                 </div>
                 </form>

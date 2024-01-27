f9c9fc1997b7394d6aed97642be6ec94fa262966
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-01-27 10:30:44 +0100

    JOIN-24 Sidebar function added

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,5 +1,11 @@
-function changeOptic(num) {
-  if (num == 1) {
-    document.getElementById('');
-  }
+function changeOptic(id) {
+  let menuItems = document.querySelectorAll('menu-category');
+
+  menuItems.forEach(function (item) {
+    item.classList.remove('active');
+  });
+
+  let clickedItem = document.getElementById(id);
+  clickedItem.classList.add('active');
+  console.log('Classlist Set to:', id);
 }

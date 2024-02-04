9514b3b187e04db86f7ccc84592e83072a85acc1
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-02-04 11:16:49 +0100

    JOIN-30 Contact Mobile
    - Fixed Bugs
    - Fixed Edit / Delete Popup
    - Fixed Topbar visible
    - Fixed z-index Option Button

--- templates/contactTemplate.js
+++ templates/contactTemplate.js
@@ -59,20 +59,24 @@ function openContactInfoHTML(contact, acronym, color, i) {
             <a id="contact-phone" href="tel:${contact.phone}">${contact.phone}</a>
         </div>
     </div>
+  `;
+}
 
-    <div id="changesMobileWrapper" onclick="closeChangesMenuMobile()">
-      <div id="changesMobile" onclick="doNotClose(event)">
-        <div class="edit" onclick="editContact(${i}, 'mobile'), openPopup('edit-contact-wrapper-mobile', 'edit-contact-mobile', 'show-overlay-menu-y')"
->
-          <img class="edit-img" src="assets/img/icons/edit.svg" alt="Edit" />
-          <p class="edit-p">Edit</p>
-        </div>
-        <div class="delete" onclick="deleteContact(${i})">
-          <img class="delete-img" src="assets/img/icons/delete.svg" alt="Delete" />
-          <p>Delete</p>
-        </div>
-      </div>
+function changesMobileHTML(i) {
+  return /*html*/ `
+  <div id="changesMobile" onclick="doNotClose(event)">
+    <div
+      class="edit"
+      onclick="editContact(${i}, 'mobile'), openPopup('edit-contact-wrapper-mobile', 'edit-contact-mobile', 'show-overlay-menu-y')"
+    >
+      <img class="edit-img" src="assets/img/icons/edit.svg" alt="Edit" />
+      <p class="edit-p">Edit</p>
     </div>
+    <div class="delete" onclick="deleteContact(${i})">
+      <img class="delete-img" src="assets/img/icons/delete.svg" alt="Delete" />
+      <p>Delete</p>
+    </div>
+  </div>
   `;
 }
 

975d7dd0f6b9ef33d608b3ccc72779d05af7e335
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-02-19 13:14:57 +0100

    JOIN-84-cssbugfix for mobile. inputfields

--- javascript/addTask.js
+++ javascript/addTask.js
@@ -39,22 +39,6 @@ function renderSubTask() {
   container.innerHTML += subTaskInputHtml();
 }
 
-// function openAddTaskPopup() {
-// document.getElementById('addTaskPopup').classList.remove('d-none');
-// document.getElementById('addTaskPopup').classList.add('slide-in');
-// }
-
-// function closeAddTaskPopup() {
-// let addTaskPopup = document.getElementById('addTaskPopup');
-// addTaskPopup.classList.remove('slide-in');
-// addTaskPopup.classList.add('slide-out');
-//
-// setTimeout(function () {
-// addTaskPopup.classList.remove('slide-out');
-// addTaskPopup.classList.add('d-none');
-// }, 900);
-// }
-
 async function addTask() {
   console.log('tasks');
   await pushAddTask();

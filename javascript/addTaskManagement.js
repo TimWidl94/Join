8b64422317fd1821b3d8dfafc7e3cb74ea2a5ab8
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-02-29 08:35:14 +0100

    JOIN-102_subtask-enter
    - Added new eventlistener, but not finished yet

--- javascript/addTaskManagement.js
+++ javascript/addTaskManagement.js
@@ -27,9 +27,24 @@ async function init() {
   setMinDateToday('myDateInput');
   await setNumberOnContacts();
   // setupEnterKeyListener();
-  setupEnterKeyListenerNew();
+  // setupEnterKeyListenerNew();
   console.log('addTask reloaded');
 }
+
+window.addEventListener(
+  'keydown',
+  function (e) {
+    if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+      if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
+        e.preventDefault();
+        console.log('setupEnterKeyListener');
+        addSubTask('subTaskInput', 'subTaskContainer');
+        return false;
+      }
+    }
+  },
+  true
+);
 
 function setupEnterKeyListenerNew() {
   document.addEventListener('DOMContentLoaded', function () {
